2d24af6ea21e1e89c0de1fdb7c5a23ea2edac3b8
Silas Harvey <silasharvey@icloud.com>
2026-03-19 21:14:20 +0000

    Cleanup settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -10,22 +10,18 @@ vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 
 -- set indentation options
-vim.opt.tabstop = 4        -- Number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 4     -- Number of spaces for autoindent (>>, <<, etc.)
+vim.opt.tabstop = 4        -- Number of spaces in a <Tab>
+vim.opt.shiftwidth = 4     -- Number of spaces for autoindent
 vim.opt.expandtab = true   -- Use spaces instead of tabs
 vim.opt.autoindent = true  -- Copy indent from current line when starting a new line
-vim.opt.smartindent = true -- Smart autoindent (improves on autoindent for code)
-vim.opt.softtabstop = 4    -- Number of spaces for <Tab> in insert mode (backspace deletes 4 spaces)
+vim.opt.smartindent = true -- Smarter autoindent
+vim.opt.softtabstop = 4    -- Number of spaces for <Tab> in insert mode (including backspace)
 
 -- keymaps
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
 vim.keymap.set("n", "<leader>fe", ":Telescope file_browser<CR>")
-
-vim.keymap.set("n", "<leader>ss", ":AutoSession save<CR>")
-vim.keymap.set("n", "<leader>sr", ":AutoSession restore<CR>")
-vim.keymap.set("n", "<leader>sd", ":AutoSession delete<CR>")
 
 vim.keymap.set("n", "<leader>q", ":bdelete<CR>")     -- close current buffer
 vim.keymap.set("n", "=", ":BufferLineCycleNext<CR>") -- next buffer
