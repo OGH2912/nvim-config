faaa90cc02aadefd03ebd1bc590343d45e4614f0
Silas Harvey <silasharvey@icloud.com>
2026-03-19 23:23:33 +0000

    Add copy over SSH

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -231,6 +231,23 @@ require("lazy").setup({
 
     alpha.setup(dashboard.config)
   end
+},
+
+{
+  "ojroques/nvim-osc52",
+  config = function()
+    require("osc52").setup({
+      max_length = 0,
+      silent = false,
+      trim = false,
+    })
+
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = function()
+        require("osc52").copy_register('"')
+      end,
+    })
+  end
 }
 
 })
